9dca9437a867cd9db5755ab1d09eae6605b7e668
Fabian Andres Levin <fabianlevin60@gmail.com>
2020-06-02 01:15:42 -0300

    First section completed - adding class component example

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# react-tutorial
-React tutorial
+# React Tutorial
+
+React tutorial from Udemy [Modern React with Redux [2020 Update]](https://www.udemy.com/course/react-redux/). 
+
+* Section 1 - Let's dive in (Completed)
+* Section 2 - Building content with JSX (undergoing)
+
+
+
+
---
+++ Section 1 - Lets dive in/classcomponents/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="ui container">
+        <div id="card" className="ui piled segment">
+          <h1>Class component example</h1>
+          <p>
+            Mollit tempor ullamco sit anim amet minim sit dolor sit proident. Do ad minim nisi dolor officia non non aliqua et consequat aliqua. 
+            Nisi cillum adipisicing magna enim amet culpa fugiat aliquip. Ullamco id sunt cupidatat do anim laboris officia non ea ut.
+          </p>
+          <p>
+            Mollit tempor ullamco sit anim amet minim sit dolor sit proident. Do ad minim nisi dolor officia non non aliqua et consequat aliqua. 
+            Nisi cillum adipisicing magna enim amet culpa fugiat aliquip. Ullamco id sunt cupidatat do anim laboris officia non ea ut.
+          </p>
+          <p>
+            Mollit tempor ullamco sit anim amet minim sit dolor sit proident. Do ad minim nisi dolor officia non non aliqua et consequat aliqua. 
+            Nisi cillum adipisicing magna enim amet culpa fugiat aliquip. Ullamco id sunt cupidatat do anim laboris officia non ea ut.
+          </p>
+        </div>
+      </div>
+  );
+}
+
+export default App;
