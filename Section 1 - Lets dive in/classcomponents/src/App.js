06164bd47a35a9fbd07b1a6375908bc370bfd682
Fabian Andres Levin <fabianlevin60@gmail.com>
2020-06-14 18:26:32 -0300

    Finished section 2 exercises and refactored projects from section 1

--- Section 1 - Lets dive in/classcomponents/src/App.js
+++ Section 1 - Lets dive in/classcomponents/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
+    return (
       <div className="ui container">
-        <div id="card" className="ui piled segment">
+        <div id="card" className="ui raised segment">
+          <a class="ui blue ribbon label" href='/#'>Section 1: Class component</a>
           <h1>Class component example</h1>
           <p>
             Mollit tempor ullamco sit anim amet minim sit dolor sit proident. Do ad minim nisi dolor officia non non aliqua et consequat aliqua. 
@@ -20,7 +22,7 @@ function App() {
           </p>
         </div>
       </div>
-  );
+  )}
 }
 
 export default App;
