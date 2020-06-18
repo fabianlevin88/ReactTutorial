1d0968b7e1cd3baa7dad81f8f6090e9af1905914
Fabian Andres Levin <fabianlevin60@gmail.com>
2020-06-17 21:42:43 -0300

    Section 5 completed

--- Section 5 - Refactoring from Functional to Class Components/seasonsv2/src/App.js
+++ Section 5 - Refactoring from Functional to Class Components/seasonsv2/src/App.js
@@ -6,24 +6,45 @@ class App extends React.Component {
     super(props);
     
     this.state = {
-      latitude: null
-    }
+      latitude: null,
+      errorMessage: null
+    };
 
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ latitude: position.coords.latitude }),
-      error => console.log(error)
+      err => this.setState({ errorMessage: err.message })
     );
   }
+  
 
   render() {
-    return (
-      <div className="ui container">
+    if (this.state.errorMessage && !this.state.latitude) {
+      return (
+        <div className="ui container">
+        <div className="ui raised segment">
+          <a id="ribbon" className="ui blue ribbon label" href='/#'>Section 5 - Refactoring from Functional to Class Components</a>
+          <h4 className="ui red header">{this.state.errorMessage}</h4>
+        </div>
+      </div>
+      )
+    } else if (!this.state.errorMessage && this.state.latitude) {
+      return (
+        <div className="ui container">
         <div className="ui raised segment">
           <a id="ribbon" className="ui blue ribbon label" href='/#'>Section 5 - Refactoring from Functional to Class Components</a>
           <h1>Latitude: {this.state.latitude}</h1>
         </div>
       </div>
-    );
+      )
+    }
+    return (
+      <div className="ui container">
+        <div className="ui raised segment">
+          <a id="ribbon" className="ui blue ribbon label" href='/#'>Section 5 - Refactoring from Functional to Class Components</a>
+          <div class="ui active centered inline loader"></div>
+        </div>
+      </div>
+    )
   }
 }
 
